d073ad1623acf847946574493f7118940fc21984
chiyuan1126 <chiyuan1126@gmail.com>
2018-12-03 12:19:50 -0500

    fix class serialized issue

---
+++ criteria2query/src/main/java/edu/columbia/dbmi/ohdsims/pojo/RuleBasedModels.java
@@ -0,0 +1,27 @@
+package edu.columbia.dbmi.ohdsims.pojo;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.hankcs.algorithm.AhoCorasickDoubleArrayTrie;
+
+public class RuleBasedModels implements Serializable{
+	private static final long serialVersionUID = 901126911124L;
+	AhoCorasickDoubleArrayTrie<String> acdat = new AhoCorasickDoubleArrayTrie<String>();
+	Map<String,String> dir=new HashMap<String,String>();
+	public AhoCorasickDoubleArrayTrie<String> getAcdat() {
+		return acdat;
+	}
+	public void setAcdat(AhoCorasickDoubleArrayTrie<String> acdat) {
+		this.acdat = acdat;
+	}
+	public Map<String, String> getDir() {
+		return dir;
+	}
+	public void setDir(Map<String, String> dir) {
+		this.dir = dir;
+	}
+	
+	
+}
